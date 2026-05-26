76075d1538bb127fa7dc30dc6aceb9bdf058b760
m-kanbara228 <masya868686@gmail.com>
2026-05-26 22:20:21 +0900

    mobileは使用しないので削除

--- frontend/mobile/lib/features/auth/providers/deep_link_state_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-enum DeepLinkState {
-  passwordRecovery,
-  emailChangeConfirmed,
-}
-
-final deepLinkStateProvider = StateProvider<DeepLinkState?>((ref) => null);
